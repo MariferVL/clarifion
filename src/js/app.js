f13bb2ca480d02815b5d6e7e5243705eafbb83ea
Marifer Villalobos <mfvl.1186@gmail.com>
2023-10-06 20:46:46 -0300

    add files

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCxlCiq4frngn2w6j3Y3Ux0SdmVuMIYACw",
+    authDomain: "clarifion-33.firebaseapp.com",
+    projectId: "clarifion-33",
+    storageBucket: "clarifion-33.appspot.com",
+    messagingSenderId: "548583898881",
+    appId: "1:548583898881:web:f17067604a974fb65ae500"
+
+};
+
+
+
+const app = initializeApp(firebaseConfig);
